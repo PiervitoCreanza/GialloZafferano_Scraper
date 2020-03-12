abb9287a2bb9829df62c584240ed8282c6d45426
piervito <piervito@MBP-di-piervito.station>
2020-03-12 18:57:35 +0100

    Express implementatio

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
-import {recipesFromIngredient} from './functions'
+let gz = require('./functions.js')
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -16,12 +21,35 @@ app.get('/', function(req, res) {
     res.send('index');
 });
 
-app.get('/ingredient', async function(req, res) {
-
-    let result = await recipesFromIngredient('pomodoro')
-    res.send(result);
+app.post('/api', async function(req, res) {
+    switch (req.body.action) {
+        case 'recipesFromIngredient':
+            if (!req.body.ingredient) {
+                res.status(400)
+                res.send({'error': 'missing parameter', 'message': 'You must specify the ingredient you want to search for.'})
+            } else {
+                let result = await gz.recipesFromIngredient(req.body.ingredient)
+                res.send(result);
+            }            
+            break;
+        
+        case 'recipeFromUrl':
+            if (!req.body.url) {
+                res.status(400)
+                res.send({'error': 'missing parameter', 'message': 'You must specify the url of the recipe.'})
+            } else {
+                let result = await gz.recipeFromUrl(req.body.url)
+                res.send(result);
+            }            
+            break;
+    
+        default:
+            res.status(400)
+            res.send({'error': 'missing parameter', 'message': 'You must specify the action you want to perform.'})
+            break;
+    }   
 });
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('The app is running on http://localhost:' + port);
 });
